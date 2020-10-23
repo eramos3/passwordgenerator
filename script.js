a63c0ca2e168d72aead3e404179e47653479de5d
eramos3 <cisco@Eders-MacBook-Pro.local>
2020-10-23 15:36:30 -0700

    fixed final issues with password gen

--- script.js
+++ script.js
@@ -7,49 +7,65 @@ var characters = {
   lowerCase: "abcdefghijklmnopqrstuvwxyz",
   special: "!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 }
-// stores user length choice
-var userLength;
 
-// stores user character choices
-var userChoice = "";
 
 // password generator function
 function generatePassword() {
+  // stores user length choice
+  var userLength = 0;
+
+  //sets conditions to false
+  var userLower = false;
+  var userNumbers = false;
+  var userSpecial = false;
+  var userUpper = false;
+  // stores user character choices
+  var userChoice = "";
+
   userLength = window.prompt("How long do you want your password to be? Choose a number bewtween 8 and 128");
   console.log(userLength);
   if (userLength < 8 || userLength > 128 || isNaN(userLength)) {
     window.alert("Wrong choice, please try again");
-    return ;
-  }
-  // ask user if they want to include one or all 4 choices in their password
-  var userNumbers = window.confirm("Do you want numbers in your password? Click OK ");
-  if (userNumbers) {
-    userChoice = userChoice + characters.numbers;
-  }
-  var userUpper = window.confirm("Do you want upper case letters? Click OK");
-  if (userUpper) {
-    userChoice = userChoice + characters.upperCase;
+    generatePassword();
+  } else {
+    // ask user if they want to include one or all 4 choices in their password
+    userNumbers = window.confirm("Do you want numbers in your password? Click OK ");
+    if (userNumbers) {
+      userChoice = userChoice + characters.numbers;
+    }
+    userUpper = window.confirm("Do you want upper case letters? Click OK");
+    if (userUpper) {
+      userChoice = userChoice + characters.upperCase;
+    }
+
+    userLower = window.confirm("Do you want lower case letters? Click OK");
+    if (userLower) {
+      userChoice = userChoice + characters.lowerCase;
+    }
+    userSpecial = window.confirm("Do you want special characters? Click OK");
+    if (userSpecial) {
+      userChoice = userChoice + characters.special;
+    }
+    if (userNumbers != true && userUpper != true && userLower != true && userSpecial != true) {
+      window.alert("You need to pick at least one choice");
+      generatePassword();
+    }
+    console.log(userChoice);
   }
 
-  var userLower = window.confirm("Do you want lower case letters? Click OK");
-  if (userLower) {
-    userChoice = userChoice + characters.lowerCase;
-  }
-  var userSpecial = window.confirm("Do you want special characters? Click OK");
-  if (userSpecial) {
-    userChoice = userChoice + characters.special;
-  }
-  console.log(userChoice);
 
   // where random password will be stored
   var random = "";
-
   // randomize password function using characters stored in userChoice
   for (var i = 0; i < userLength; i++) {
     random = random + userChoice.charAt(Math.floor(Math.random() * userChoice.length));
+
   }
-  return (random)
+  return (random);
+
+
 };
+
 
 
 
